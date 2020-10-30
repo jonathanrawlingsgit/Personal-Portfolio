e7a84cde61949216c2feda83cc5de1e4e561961d
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2020-10-30 10:31:38 -0700

    Landing page & styling

---
+++ src/components/landingpage.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Grid, Cell } from 'react-mdl'
+
+class Landing extends Component {
+    render() {
+        return (
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className='landing-grid'>
+                    <Cell col={12}>
+                        <img 
+                          src="https://www.btcpa.net/content/uploads/2020/04/Male-Avatar.jpg"
+                          alt="avatar"
+                          className="avatar-img"
+                          />
+
+                          <div className="banner-text">
+                              <h1>Full Stack Web Developer</h1>
+
+                              <hr/>
+
+                              <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | Ruby on Rails | SQL</p>
+                              <div className="social-links">
+
+                                  {/* LinkedIn */}
+                                  <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                      <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                  </a>
+
+                                  {/* Github */}
+                                  <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                      <i className="fa fa-github-square" aria-hidden="true" />
+                                  </a>
+                                 
+                                 {/* Youtube */}
+                                 <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                      <i className="fa fa-youtube-square" aria-hidden="true" />
+                                  </a>
+                                
+
+                              </div>
+                          </div>
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
+}
+export default Landing;
